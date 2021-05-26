6bec8971247e26390fac9fc0de15838675ef2995
Hamlin Liu <hamlin.liu@gmail.com>
2021-05-25 19:36:49 -0700

    Initial commit to new branch

--- PPO.py
+++ PPO.py
@@ -1,9 +1,43 @@
+
+
+
+
+
+class RolloutBuffer:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.is_terminals = []
+    
+
+    def clear(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+        del self.is_terminals[:]
+
+
 class PPO:
-	def__init__(self, state_dim,action_dim,lrate_actor,lrate_critic, gamma, epochs, epsilon, has_continuous_action_space)
+	def __init__(self, 
+				state_dim, 
+				action_dim, 
+				lrate_actor,
+				lrate_critic, 
+				gamma, 
+				epochs, 
+				epsilon, 
+				has_continuous_action_space):
+	"""
+	Initializer for the 
+	"""
 
 
-	def select_action(self,)
+
+	def select_action(self,):
 
 
-	def update(self,)
+	def update(self,):
 	#estimated retuns using Monte Carlo	
